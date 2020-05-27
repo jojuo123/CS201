8e67183eeb02cf4b407dc8e832dc1902f8757356
vtduy3100 <vtduy3100@gmail.com>
2020-05-27 15:02:22 +0700

    csim.c

--- cachelab-handout/csim.c
+++ cachelab-handout/csim.c
@@ -6,6 +6,7 @@
 #include <stdlib.h>
 #define MISS 0
 #define HIT 1
+#define EVICTION 2
 #define IGNORE -1
 #define LOAD 0
 #define MODIFY 1
@@ -115,9 +116,157 @@ void Init()
     }
 }
 
-void HandleQuery(int instruction, int address, int size)
+uint getTag(ull address)
 {
-    printf("%d %d %d\n",instruction,address,size);
+    return (uint)(address >> (64 - tagBit));
+}
+
+uint getSet(ull address)
+{
+    return (uint)((address << tagBit) >> (tagBit+blockBit));
+}
+
+uint Access(uint tag, sSet* set)
+{
+    uint result = EVICTION;
+    for (uint i = 0; i < Eway; ++i)
+    {
+        //update time
+        ++(set->way[i].time);
+        if (set->way[i].valid != 0 && set->way[i].tag == tag)
+        {
+            set->way[i].time = 0;
+            result = HIT;
+        }
+    }
+    //if (result == HIT) return result;
+    
+    
+    if (result != HIT) 
+    //if there is an invalid one
+        for (uint i = 0; i < Eway; ++i)
+            if (set->way[i].valid == 0) 
+            {
+                set->way[i].valid = 1;
+                set->way[i].tag = tag;
+                set->way[i].time = 0;
+                result = MISS;
+            }
+
+    //if everything is valid
+    if (result == EVICTION)
+    {
+        set->way[set->LRU].tag = tag;
+        set->way[set->LRU].time = 0;
+    }
+
+    //update LRU
+    uint max = 0; uint imax = 0;
+    for (uint i = 0; i < Eway; ++i)
+        if (set->way[i].time > max) { max = set->way[i].time; imax = i; }
+    set->LRU = imax;
+
+    return result;
+}
+
+uint LoadAndStore(ull address)
+{
+    uint set = getSet(address);
+    uint tag = getTag(address);
+    
+    uint val = Access(tag, cache.s+set);
+    switch (val)
+    {
+        case MISS:
+            ++result[MISS];
+            return MISS;
+        case HIT:
+            ++result[HIT];
+            return HIT;
+        case EVICTION:
+            ++result[MISS];
+            ++result[EVICTION];
+            return EVICTION;
+    }
+}
+
+uint Modify(ull address)
+{
+    uint set = getSet(address);
+    uint tag = getTag(address);
+    uint ret;
+    uint val = Access(tag, cache.s+set);
+    switch (val)
+    {
+        case MISS:
+            ++result[MISS];
+            break;
+        case HIT:
+            ++result[HIT];
+            break;
+        case EVICTION:
+            ++result[MISS];
+            ++result[EVICTION];
+            break;
+    }
+    ret = val;
+    
+    val = Access(tag, cache.s+set);
+    switch (val)
+    {
+        case MISS:
+            ++result[MISS];
+            break;
+        case HIT:
+            ++result[HIT];
+            break;
+        case EVICTION:
+            ++result[MISS];
+            ++result[EVICTION];
+            break;
+    }
+
+    return val;
+}
+
+
+
+void HandleQuery(char instruction, ull address, uint size, uint verbose)
+{
+    //printf("%d %d %d\n",instruction,address,size);
+    uint opt;
+    switch (instruction)
+    {
+        case 'L':
+        case 'S':
+            opt = LoadAndStore(address);
+            if (verbose == 0)
+                break;
+            else 
+            {
+                if (opt == HIT)
+                    printf("%c %llx,%d hit\n",instruction, address, size);
+                else if (opt == MISS)
+                    printf("%c %llx,%d miss\n",instruction, address, size);
+                else 
+                    printf("%c %llx,%d miss eviction\n",instruction, address, size);
+            }
+            break;
+        case 'M':
+            opt = Modify(address);
+            if (verbose == 0)
+                break;
+            else 
+            {
+                if (opt == HIT)
+                    printf("%c %llx,%d hit hit\n",instruction, address, size);
+                else if (opt == MISS)
+                    printf("%c %llx,%d miss hit\n",instruction, address, size);
+                else 
+                    printf("%c %llx,%d miss eviction hit\n",instruction, address, size);
+            }
+            break;
+    }
 }
 
 int getInstruction(char* temp)
@@ -147,30 +296,20 @@ void inputHandle()
     if (fp == NULL)
     {
         printf("File not found \n");
+        return;
     }
     while (fgets(buf, 255, (FILE*)fp) != NULL)
     {
         strtok(buf, "\n");
         char temp;
-        int instruction;
+        uint instruction;
         ull address;
-        int size;
+        uint size;
         if (buf[0] == '0') continue;
         sscanf(buf, " %c %llx,%d", &temp, &address, &size);
-        instruction = getInstruction(&temp);
-        // temp = strtok(buf," ,");
-        // if (temp == NULL) continue;
-        // instruction = getInstruction(temp);
-        
-        // temp = strtok(NULL, " ,");
-        // if (temp == NULL) continue;
-        // address = strtol(temp, NULL, 0);
-
-        // temp = strtok(NULL, " ,");
-        // if (temp == NULL) continue;
-        // size = strtol(temp, NULL, 10);
-        
-        HandleQuery(instruction, address, size);
+        //instruction = getInstruction(&temp);
+        //if (verbose == 1)
+        HandleQuery(temp, address, size, verbose);
     }
     fclose(fp);
 }
@@ -179,7 +318,7 @@ int main(int argc, char **argv)
 {
     getFlag(argc, argv);
     Init();
-    //inputHandle();
+    inputHandle();
     printSummary(0, 0, 0);
     Halt();
     return 0;
